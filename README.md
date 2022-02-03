4ed392b7073e629f7a248c5263da6ac825320462
admin <shethinksnyc@protonmail.com>
2022-02-03 17:09:14 -0500

    soft reset

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Starting repos from command line
 Eliminating bash error message after Mac upgrade
 setting default shell
-detached head from *master  >
+detached head from *master
+initializing package JSON file
+using express handlebars with common JS
  -->
+
 # server_side_validation_form
--- app.js
+++ app.js
@@ -1,63 +1,61 @@
 "use strict";
+
+// dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
+const path = require("path");
 const nodemailer = require("nodemailer");
-const http = require("http");
+const ProtonMail = require("protonmail-api");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+// initialize app variable
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/send") {
-    let body = [];
-    // capture message
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      // converts buffer stream into readable string format
-      let parsedBody = Buffer.concat(body).toString(); //
-      sendEmail().catch(console.error);
-    });
-  }
+// View engine set up
+app.engine("handlebars", engine({ extname: ".hbs", defaultLayout: "" }));
+app.set("view engine", "handlebars");
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+// Static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+//bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//create route
+app.get("/", (req, res) => {
+  res.render("contact");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+//post route for submission
+app.post("/send", (req, res) => {
+  let output = `
+  <p>You have a new message</p>
+  <h3>Contact Details</h3>
+  <ul> 
+  <i>Name: ${req.body.name}</i>
+  <i>Email: ${req.body.email}</i>
+  </ul>
+  <h3>Message: <p>${req.body.message}</p></h3>
+`;
 });
 
-// async..await is not allowed in global scope, must use a wrapper
-async function sendEmail() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
+(async () => {
+  const pm = await ProtonMail.connect({
+    username: "oneMarketingStudios@protonmail.com",
+    password: "Bullshit6868$",
   });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <shethinksnyc@protonmail.com>', // sender address
-    to: "nnikkhoui@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+  await pm.sendEmail({
+    to: "shethinksnyc@protonmail.com",
+    subject: "Nodemailer Contact",
+    body: output,
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  pm.close();
+})();
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+//   setupemail data with unicode
+// listen on a port
+app.listen(3000, () => console.log("Server started..."));
